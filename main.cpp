b08a74a7f3f418ec7accad20e2a287d8919ce774
Andreas Mallios <andreas.mallios@gmail.com>
2022-02-06 17:08:50 +0100

    add user I/O menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    while(true) {
+        std::cout << "1: Print help" << std::endl;
+        std::cout << "2: Print exchange stats" << std::endl;
+        std::cout << "3: Place an ask" << std::endl;
+        std::cout << "4: Place a bid" << std::endl;
+        std::cout << "5: Print wallet" << std::endl;
+        std::cout << "6: Continue" << std::endl;
+
+        int userOption;
+        std::cout << "Type in 1-6" << std::endl;
+        std::cin >> userOption;
+
+        switch (userOption) {
+            case 1:
+                std::cout << "1: Print help" << std::endl;
+                break;
+            case 2:
+                std::cout << "2: Print exchange stats" << std::endl;
+                break;
+            case 3:
+                std::cout << "3: Place an ask" << std::endl;
+                break;
+            case 4:
+                std::cout << "4: Place a bid" << std::endl;
+                break;
+            case 5:
+                std::cout << "5: Print wallet" << std::endl;
+                break;
+            case 6:
+                std::cout << "6: Continue" << std::endl;
+                break;
+            default:
+                std::cout << "Invalid input. Type 1-6" << std::endl;
+        }
+    }
 }
