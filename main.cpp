61d13dfe9f9223bf8ca2282d9d8bd0a0d07b23a3
Andreas Mallios <andreas.mallios@gmail.com>
2022-02-06 17:18:15 +0100

    convert menu to functions

--- main.cpp
+++ main.cpp
@@ -1,39 +1,74 @@
 #include <iostream>
 
+void printMenu() {
+    std::cout << "1: Print help" << std::endl;
+    std::cout << "2: Print exchange stats" << std::endl;
+    std::cout << "3: Place an ask" << std::endl;
+    std::cout << "4: Place a bid" << std::endl;
+    std::cout << "5: Print wallet" << std::endl;
+    std::cout << "6: Continue" << std::endl;
+}
+
+void printHelp() {
+    std::cout << "1: Print help" << std::endl;
+}
+
+void printMarketStats() {
+    std::cout << "2: Print exchange stats" << std::endl;
+}
+
+void enterAsk() {
+    std::cout << "3: Place an ask" << std::endl;
+}
+
+void enterBid() {
+    std::cout << "4: Place a bid" << std::endl;
+}
+
+void printWallet() {
+    std::cout << "5: Print wallet" << std::endl;
+}
+
+void gotoNextTimeframe() {
+    std::cout << "6: Continue" << std::endl;
+}
+
+int getUserOption() {
+    int userOption;
+    std::cout << "Type in 1-6" << std::endl;
+    std::cin >> userOption;
+    return userOption;
+}
+
+void processUserOption(int userOption) {
+    switch (userOption) {
+        case 1:
+            printHelp();
+            break;
+        case 2:
+            printMarketStats();
+            break;
+        case 3:
+            enterAsk();
+            break;
+        case 4:
+            enterBid();
+            break;
+        case 5:
+            printWallet();
+            break;
+        case 6:
+            gotoNextTimeframe();
+            break;
+        default:
+            std::cout << "Invalid input. Type 1-6" << std::endl;
+    }
+}
+
 int main() {
     while(true) {
-        std::cout << "1: Print help" << std::endl;
-        std::cout << "2: Print exchange stats" << std::endl;
-        std::cout << "3: Place an ask" << std::endl;
-        std::cout << "4: Place a bid" << std::endl;
-        std::cout << "5: Print wallet" << std::endl;
-        std::cout << "6: Continue" << std::endl;
-
-        int userOption;
-        std::cout << "Type in 1-6" << std::endl;
-        std::cin >> userOption;
-
-        switch (userOption) {
-            case 1:
-                std::cout << "1: Print help" << std::endl;
-                break;
-            case 2:
-                std::cout << "2: Print exchange stats" << std::endl;
-                break;
-            case 3:
-                std::cout << "3: Place an ask" << std::endl;
-                break;
-            case 4:
-                std::cout << "4: Place a bid" << std::endl;
-                break;
-            case 5:
-                std::cout << "5: Print wallet" << std::endl;
-                break;
-            case 6:
-                std::cout << "6: Continue" << std::endl;
-                break;
-            default:
-                std::cout << "Invalid input. Type 1-6" << std::endl;
-        }
+        printMenu();
+        processUserOption(getUserOption());
     }
+    return 0;
 }
